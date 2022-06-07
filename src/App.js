37b5126c31b0959113437f43a24c8da83891d282
Jorge Bojacá <jorgebojaca@gmail.com>
2022-06-06 23:17:08 -0500

    Se configura el componente principal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { LoaderConsole } from './LoaderConsole/LoaderConsole';
+import { LoginConsole } from './LoginConsole/LoginConsole';
+import Wallet from './Wallet/Wallet';
 
-function App() {
+export const App = () => {
+  const { uid } = useSelector(({ auth }) => auth);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <LoaderConsole />
+      {uid ? <Wallet /> : <LoginConsole />}
+    </>
   );
-}
-
-export default App;
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { App } from './App';
+import { consoleMessage } from './consoleMessage';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+consoleMessage(
+  '¡ALERTA!',
+  'No escriba ningún código aquí, esta área es solo para desarrolladores. (Podrían tener acceso a su información si lo hace)'
+);
