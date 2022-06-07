19640b7ecc531fa416cd1fc355e4eadc2da7556a
Jorge Bojacá <jorgebojaca@gmail.com>
2022-06-06 23:36:33 -0500

    Se crea componentes 'Wallet' que se encarga de mostrar la billetera con formulario y contraseñas, y 'PassApp' encargado de manejar una constraseña en específico

---
+++ src/Wallet/PassApp.js
@@ -0,0 +1,145 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import enc_utf8 from 'crypto-js/enc-utf8';
+import aes from 'crypto-js/aes';
+import { useDispatch, useSelector } from 'react-redux';
+import { removeApp } from '../reducers/walletSlice';
+
+const PassApp = ({
+  app,
+  resetBy,
+  idx,
+  onClick,
+  setResetBy,
+  showDelete,
+  setShowDelete,
+}) => {
+  const ref = useRef();
+  const [opt, setOpt] = useState(0);
+  const [key, setKey] = useState('');
+  const [isCopied, setIsCopied] = useState(false);
+  const { uid } = useSelector(({ auth }) => auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (opt !== 0 && opt !== 3 && idx !== resetBy) {
+      //Oculta la contraseña inmediatamente id despues del llamado de resetBy
+      setOpt(3);
+    }
+    setIsCopied(false);
+  }, [resetBy, setOpt, idx, opt]);
+
+  useEffect(() => {
+    if (ref.current && opt === 1) {
+      ref.current.focus();
+    }
+  }, [opt]);
+
+  const handleClick = (e) => {
+    onClick(e);
+    setShowDelete(false);
+    if (opt === 0 || opt > 2) {
+      setOpt(1);
+    } else if (opt === 2) {
+      setOpt(3);
+      setResetBy(-1);
+      setKey('');
+    }
+  };
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (opt === 1 && key !== '') {
+        setOpt(2);
+      }
+    },
+    [opt, key]
+  );
+  const getPass = () => {
+    const cipher = aes.decrypt(app.value, uid + key + uid).toString(enc_utf8);
+    if (!cipher) return '😥';
+    const objCipher = JSON.parse(cipher);
+    return objCipher.val;
+  };
+
+  return (
+    <>
+      <div
+        title={app.name}
+        className={`wallet-app ${
+          opt === 0
+            ? 'wallet-app__init'
+            : opt === 1
+            ? 'wallet-app_inp'
+            : opt === 2
+            ? 'wallet-app_show_pass '
+            : opt === 3
+            ? 'wallet-app_hide_pass'
+            : ''
+        } ${resetBy !== idx && resetBy !== -1 ? 'wallet-app_opacity' : ''} ${
+          showDelete ? 'wallet-app_deleting' : ''
+        }`}
+        onClick={handleClick}
+      >
+        {showDelete && (
+          <span
+            className="wallet-app_deleting_btn"
+            title="Eliminar"
+            onClick={(e) => {
+              e.stopPropagation();
+              const res = window.confirm(
+                `¿Está seguro q desea eliminar la contraseña para ${app.name}?`
+              );
+              if (res) {
+                dispatch(removeApp(app.id));
+              }
+            }}
+          >
+            x
+          </span>
+        )}
+
+        {opt === 1 ? (
+          <form onSubmit={handleSubmit}>
+            <input
+              ref={ref}
+              placeholder="Escriba su KEY..."
+              className="wallet-app-input"
+              onChange={(e) =>
+                setKey(() => (e.target.value ? e.target.value : ''))
+              }
+            ></input>
+          </form>
+        ) : opt === 2 ? (
+          <>
+            <strong>{getPass()}</strong>
+            {!isCopied ? (
+              <button
+                style={{
+                  position: 'absolute',
+                  right: 0,
+                  cursor: 'pointer',
+                  fontSize: 'small',
+                }}
+                className="wallet-btn"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  navigator.clipboard
+                    .writeText(getPass())
+                    .then(() => setIsCopied(true));
+                }}
+              >
+                copy
+              </button>
+            ) : (
+              <span className="wallet-app_check"></span>
+            )}
+          </>
+        ) : (
+          app.name
+        )}
+      </div>
+    </>
+  );
+};
+
+export default PassApp;
---
+++ src/Wallet/Wallet.js
@@ -0,0 +1,190 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import aes from 'crypto-js/aes';
+import { fetchApps, saveApp } from '../reducers/walletSlice';
+import { logoutFirebase } from '../reducers/authSlice';
+
+// import PassApp from './PassApp';
+import './Wallet.css';
+import PassApp from './PassApp';
+
+function Wallet() {
+  const [inputCipher, setInputCipher] = useState('');
+  const [inputName, setInputName] = useState('');
+  const [showInfo, setShowInfo] = useState(false);
+  const [showDelete, setShowDelete] = useState(false);
+  const [uid, apps, backgroundColor] = useSelector((state) => [
+    state.auth.uid,
+    state.wallet.apps,
+    state.ui.backgroundColor,
+  ]);
+  const [resetBy, setResetBy] = useState(-1);
+  const [showAdd, setShowAdd] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!uid) return;
+    dispatch(fetchApps(uid));
+  }, [dispatch, uid]);
+
+  useEffect(() => {
+    if (apps.length === 0) {
+      setShowAdd(true);
+    }
+  }, [apps.length]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const key = prompt('KEY');
+    if (!key) {
+      alert('key es obligatorio');
+      return;
+    }
+    const cipher = aes.encrypt(
+      JSON.stringify({ val: inputCipher }),
+      uid + key + uid
+    );
+    dispatch(saveApp({ name: inputName, value: cipher.toString() }));
+    setInputName('');
+    setInputCipher('');
+    setShowAdd(false);
+  };
+  const handleClick = (e, idx) => {
+    e.stopPropagation();
+    setResetBy(idx);
+  };
+  const handleMouseInfo = () => {
+    setShowInfo((shI) => !shI);
+  };
+
+  return (
+    <div
+      className="wallet"
+      onMouseLeave={() => {
+        setResetBy(-1);
+      }}
+      onClick={() => {
+        setResetBy(-1);
+        setShowDelete(false);
+      }}
+    >
+      <main style={{ backgroundColor }} className="wallet-main">
+        <div>
+          <h1 className="inline">Passwords Wallet</h1>
+          {apps.length > 0 && (
+            <button
+              title={showAdd ? 'Esconder form' : 'Nueva contraseña'}
+              className="wallet-btn inline"
+              onClick={() => {
+                setShowAdd((s) => !s);
+              }}
+            >
+              {showAdd ? '^' : '+'}
+            </button>
+          )}
+        </div>
+        <button
+          tittle="Cerrar sesión"
+          style={{ position: 'absolute', top: '0', right: '0' }}
+          className="wallet-btn"
+          onClick={() => {
+            dispatch(logoutFirebase());
+          }}
+        >
+          X
+        </button>
+        {showInfo && (
+          <span className="wallet-hover-info">
+            **Recuerde que la KEY que se le pedirá se la debe aprender ya que es
+            el acceso único a la contraseña, no la guardaremos por lo cual no se
+            puede recuperar**
+          </span>
+        )}
+        {showAdd && (
+          <div
+            className="wallet-form_container"
+            style={{ position: 'relative' }}
+          >
+            <h3>Guardar Contraseña</h3>
+            <div style={{ marginBottom: '10px', textAlign: 'start' }}>
+              Escriba la contraseña que desea guardar.
+              <div
+                onMouseEnter={handleMouseInfo}
+                onMouseLeave={handleMouseInfo}
+                style={{
+                  cursor: 'pointer',
+                  display: 'inline-block',
+                  position: 'relative',
+                }}
+              >
+                (i)
+              </div>
+            </div>
+
+            <form className="wallet-form" onSubmit={(e) => handleSubmit(e)}>
+              <input
+                name="name"
+                className="wallet-input"
+                autoComplete="off"
+                placeholder="Nombre"
+                onChange={({ target: { value } }) => setInputName(value)}
+                value={inputName}
+              />
+              <input
+                name="cipher"
+                className="wallet-input"
+                autoComplete="off"
+                placeholder="Contraseña"
+                onChange={({ target: { value } }) => setInputCipher(value)}
+                value={inputCipher}
+              />
+              <button className="wallet-btn" type="submit">
+                Guardar
+              </button>
+            </form>
+          </div>
+        )}
+        {apps.length > 0 && (
+          <div
+            className={
+              showAdd
+                ? 'wallet-app_container_form_showed'
+                : 'wallet-app_container_form_hidden'
+            }
+          >
+            <h3 style={{ display: 'inline-block' }}>Contraseñas Guardadas</h3>
+            <button
+              title="Eliminar contraseña"
+              className="wallet-btn"
+              onClick={(e) => {
+                e.stopPropagation();
+                setShowDelete(true);
+                setResetBy(-1);
+              }}
+            >
+              -
+            </button>
+            <div className="wallet-app_container">
+              {apps.map((app, idx) => (
+                <PassApp
+                  key={app.name}
+                  app={app}
+                  resetBy={resetBy}
+                  setResetBy={setResetBy}
+                  showDelete={showDelete}
+                  setShowDelete={setShowDelete}
+                  idx={idx}
+                  onClick={(e, id = idx) => {
+                    handleClick(e, id);
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default Wallet;
