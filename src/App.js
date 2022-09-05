7c91cace2340562914ced79e8440a37ea6f77b7a
Jorge Bojacá <jorgebojaca@gmail.com>
2022-09-04 23:42:29 -0500

    LoginConsole changed to LoginCaptcha

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { LoaderConsole } from './LoaderConsole/LoaderConsole';
-import { LoginConsole } from './LoginConsole/LoginConsole';
+import { LoginCaptcha } from './LoginCaptcha/LoginCaptcha';
+// import { LoginConsole } from './LoginConsole/LoginConsole';
 import Wallet from './Wallet/Wallet';
 
 export const App = () => {
   const { uid } = useSelector(({ auth }) => auth);
+  useEffect(() => {
+    const recap = sessionStorage.getItem('recap');
+    if (!uid && recap) {
+      sessionStorage.removeItem('recap');
+      window.location.reload();
+    }
+  }, [uid]);
 
   return (
     <>
       <LoaderConsole />
-      {uid ? <Wallet /> : <LoginConsole />}
+      {uid ? <Wallet /> : <LoginCaptcha />}
+      {/* {uid ? <Wallet /> : <LoginConsole />} */}
     </>
   );
 };
